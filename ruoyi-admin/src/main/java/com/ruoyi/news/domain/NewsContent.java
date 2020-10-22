7e6500f096864b7c99fa00fcee5ba8347035f8ce
Mrchai521 <18636902115@163.com>
2020-10-22 15:33:37 +0800

    修改邮件发送接口

--- ruoyi-admin/src/main/java/com/ruoyi/news/domain/NewsContent.java
+++ ruoyi-admin/src/main/java/com/ruoyi/news/domain/NewsContent.java
@@ -7,73 +7,75 @@ import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
  * 新闻内容对象 news_content
- * 
+ *
  * @author ruoyi
  * @date 2020-09-22
  */
-public class NewsContent extends BaseEntity
-{
+public class NewsContent extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
-    /** 编号 */
+    /**
+     * 编号
+     */
     private Long newsContentId;
 
-    /** 新闻标题 */
+    /**
+     * 新闻标题
+     */
     @Excel(name = "新闻标题")
     private String newsContentTitle;
 
-    /** 新闻内容 */
+    /**
+     * 新闻内容
+     */
     @Excel(name = "新闻内容")
     private String newsContentText;
 
-    /** 所属菜单id */
+    /**
+     * 所属菜单id
+     */
     @Excel(name = "所属菜单id")
     private Long newsMenuId;
 
-    public void setNewsContentId(Long newsContentId) 
-    {
+    public void setNewsContentId(Long newsContentId) {
         this.newsContentId = newsContentId;
     }
 
-    public Long getNewsContentId() 
-    {
+    public Long getNewsContentId() {
         return newsContentId;
     }
-    public void setNewsContentTitle(String newsContentTitle) 
-    {
+
+    public void setNewsContentTitle(String newsContentTitle) {
         this.newsContentTitle = newsContentTitle;
     }
 
-    public String getNewsContentTitle() 
-    {
+    public String getNewsContentTitle() {
         return newsContentTitle;
     }
-    public void setNewsContentText(String newsContentText) 
-    {
+
+    public void setNewsContentText(String newsContentText) {
         this.newsContentText = newsContentText;
     }
 
-    public String getNewsContentText() 
-    {
+    public String getNewsContentText() {
         return newsContentText;
     }
-    public void setNewsMenuId(Long newsMenuId) 
-    {
+
+    public void setNewsMenuId(Long newsMenuId) {
         this.newsMenuId = newsMenuId;
     }
 
-    public Long getNewsMenuId() 
-    {
+    public Long getNewsMenuId() {
         return newsMenuId;
     }
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("newsContentId", getNewsContentId())
-            .append("newsContentTitle", getNewsContentTitle())
-            .append("newsContentText", getNewsContentText())
-            .append("newsMenuId", getNewsMenuId())
-            .toString();
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("newsContentId", getNewsContentId())
+                .append("newsContentTitle", getNewsContentTitle())
+                .append("newsContentText", getNewsContentText())
+                .append("newsMenuId", getNewsMenuId())
+                .toString();
     }
 }
--- ruoyi-admin/src/main/java/com/ruoyi/news/domain/NewsMenu.java
+++ ruoyi-admin/src/main/java/com/ruoyi/news/domain/NewsMenu.java
@@ -7,101 +7,105 @@ import com.ruoyi.common.core.domain.TreeEntity;
 
 /**
  * 新闻菜单对象 news_menu
- * 
+ *
  * @author ruoyi
  * @date 2020-09-22
  */
-public class NewsMenu extends TreeEntity
-{
+public class NewsMenu extends TreeEntity {
     private static final long serialVersionUID = 1L;
 
-    /** 编号 */
+    /**
+     * 编号
+     */
     private Long newsMenuId;
 
-    /** 父id */
+    /**
+     * 父id
+     */
     @Excel(name = "父id")
     private Long newsMenuParentId;
 
-    /** 菜单名称 */
+    /**
+     * 菜单名称
+     */
     @Excel(name = "菜单名称")
     private String newsMenuName;
 
-    /** 菜单类型 */
+    /**
+     * 菜单类型
+     */
     @Excel(name = "菜单类型")
     private String newsMenuType;
 
-    /** 菜单图标 */
+    /**
+     * 菜单图标
+     */
     @Excel(name = "菜单图标")
     private String newsMenuIcon;
 
-    /** 显示 */
+    /**
+     * 显示
+     */
     @Excel(name = "显示")
     private String newsMenuVisibel;
 
-    public void setNewsMenuId(Long newsMenuId) 
-    {
+    public void setNewsMenuId(Long newsMenuId) {
         this.newsMenuId = newsMenuId;
     }
 
-    public Long getNewsMenuId() 
-    {
+    public Long getNewsMenuId() {
         return newsMenuId;
     }
-    public void setNewsMenuParentId(Long newsMenuParentId) 
-    {
+
+    public void setNewsMenuParentId(Long newsMenuParentId) {
         this.newsMenuParentId = newsMenuParentId;
     }
 
-    public Long getNewsMenuParentId() 
-    {
+    public Long getNewsMenuParentId() {
         return newsMenuParentId;
     }
-    public void setNewsMenuName(String newsMenuName) 
-    {
+
+    public void setNewsMenuName(String newsMenuName) {
         this.newsMenuName = newsMenuName;
     }
 
-    public String getNewsMenuName() 
-    {
+    public String getNewsMenuName() {
         return newsMenuName;
     }
-    public void setNewsMenuType(String newsMenuType) 
-    {
+
+    public void setNewsMenuType(String newsMenuType) {
         this.newsMenuType = newsMenuType;
     }
 
-    public String getNewsMenuType() 
-    {
+    public String getNewsMenuType() {
         return newsMenuType;
     }
-    public void setNewsMenuIcon(String newsMenuIcon) 
-    {
+
+    public void setNewsMenuIcon(String newsMenuIcon) {
         this.newsMenuIcon = newsMenuIcon;
     }
 
-    public String getNewsMenuIcon() 
-    {
+    public String getNewsMenuIcon() {
         return newsMenuIcon;
     }
-    public void setNewsMenuVisibel(String newsMenuVisibel) 
-    {
+
+    public void setNewsMenuVisibel(String newsMenuVisibel) {
         this.newsMenuVisibel = newsMenuVisibel;
     }
 
-    public String getNewsMenuVisibel() 
-    {
+    public String getNewsMenuVisibel() {
         return newsMenuVisibel;
     }
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("newsMenuId", getNewsMenuId())
-            .append("newsMenuParentId", getNewsMenuParentId())
-            .append("newsMenuName", getNewsMenuName())
-            .append("newsMenuType", getNewsMenuType())
-            .append("newsMenuIcon", getNewsMenuIcon())
-            .append("newsMenuVisibel", getNewsMenuVisibel())
-            .toString();
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("newsMenuId", getNewsMenuId())
+                .append("newsMenuParentId", getNewsMenuParentId())
+                .append("newsMenuName", getNewsMenuName())
+                .append("newsMenuType", getNewsMenuType())
+                .append("newsMenuIcon", getNewsMenuIcon())
+                .append("newsMenuVisibel", getNewsMenuVisibel())
+                .toString();
     }
 }
